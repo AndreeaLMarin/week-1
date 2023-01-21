5f31c818844949d2a17a075e2e127d428af077e6
Andreea Marin <118391288+AndreeaLMarin@users.noreply.github.com>
2023-01-21 14:10:16 +0000

    Initial commit

---
+++ basics/output/multiline_message.py
@@ -0,0 +1,20 @@
+
+print("System Failure Imminent!")
+print("System reboot has been initiated...")
+print()
+print("...rebooting sensory system")
+print("...rebooting output motors")
+print("...rebooting hover engine")
+print()
+print("System online.")
+
+print("""
+System Failure Imminent!
+System reboot has been initiated...
+
+...rebooting sensory system
+...rebooting output motors
+...rebooting hover engine
+
+System online.
+""")
